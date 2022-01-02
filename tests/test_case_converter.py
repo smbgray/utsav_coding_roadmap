3af5204dae192003cdd524d354664f042cf495a8
smbgray <smbgray@gmail.com>
2022-01-02 14:29:19 +0100

    add camel converter

--- tests/test_case_converter.py
+++ tests/test_case_converter.py
@@ -2,4 +2,7 @@ import pytest
 import coding_roadmap.manipulate_strings as cr
 
 def test_convert_to_camel():
-    assert cr.convert("Hello, World.","camel") == "helloWorld"
+    assert cr.camel_converter("Hello, World.") == "helloWorld"
+
+def test_clean_up ():
+    assert cr.clean_up("remove spaces and others,;/:") == "removespacesandothers"
