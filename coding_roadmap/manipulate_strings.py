cd7dc71cc935d1b3cdeb1e29c1f0ca04ab3141f2
smbgray <smbgray@gmail.com>
2022-01-02 14:28:58 +0100

    add camel converter

--- coding_roadmap/manipulate_strings.py
+++ coding_roadmap/manipulate_strings.py
@@ -1,2 +1,16 @@
+import re
+
 def convert(input: str, case: str) -> str:
+    
     return ""
+
+def clean_up (input: str) -> str:
+    
+    cleaned_string = re.sub(r'\W+','', input)
+    return cleaned_string
+
+def camel_converter(input: str) -> str:
+    res = input.split()
+    result = [word.capitalize() for word in res ]
+    result[0] = result[0].lower()
+    return clean_up(''.join(str(word) for word in result ))
