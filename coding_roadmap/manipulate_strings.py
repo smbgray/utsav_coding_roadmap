cd9a88d2199ce784d18e31dd2f8d5f4acb998dac
smbgray <smbgray@gmail.com>
2022-01-02 14:34:29 +0100

    reformat

--- coding_roadmap/manipulate_strings.py
+++ coding_roadmap/manipulate_strings.py
@@ -1,16 +1,17 @@
+"""String manipulations module"""
 import re
 
 def convert(input: str, case: str) -> str:
-    
+    """Main flow function"""
     return ""
 
 def clean_up (input: str) -> str:
-    
+    """Clean up string from nonalphanumerical characters"""
     cleaned_string = re.sub(r'\W+','', input)
     return cleaned_string
 
 def camel_converter(input: str) -> str:
-    res = input.split()
-    result = [word.capitalize() for word in res ]
+    """Convert into camel case"""
+    result = [word.capitalize() for word in input.split() ]
     result[0] = result[0].lower()
     return clean_up(''.join(str(word) for word in result ))
